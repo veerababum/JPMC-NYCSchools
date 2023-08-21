1dd9d9d7ec464ca36ed30bea2c8744b410f65ca1
Vmulugu1 <veerababu.mulugu1@t-mobile.com>
2023-08-20 23:43:09 -0500

    Service function updated to single function

--- 19860706-VeerababuMulugu-NYCSchools/19860706-VeerababuMulugu-NYCSchools/Services/SchoolService.swift
+++ 19860706-VeerababuMulugu-NYCSchools/19860706-VeerababuMulugu-NYCSchools/Services/SchoolService.swift
@@ -10,37 +10,9 @@ import Foundation
 class SchoolService {
     let schoolServiceURL = "https://data.cityofnewyork.us/resource/f9bf-2cp4.json"
     let schoolDetailsServiceURL = "https://data.cityofnewyork.us/resource/s3k6-pzi2.json"
-    
-    func getSchools(completionHandler: @escaping (Result<[School], Error>) -> Void) {
-        guard let url = URL(string: schoolServiceURL) else {
-            completionHandler(.failure(NSError(domain: "", code: 404, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
-            return
-        }
-        
-        let session = URLSession.shared
-        session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                completionHandler(.failure(error))
-                return
-            }
-            
-            guard let data = data else {
-                completionHandler(.failure(NSError(domain: "", code: 500, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
-                return
-            }
-            
-            do {
-                let jsonDecoder = JSONDecoder()
-                let responseModel = try jsonDecoder.decode([School].self, from: data)
-                completionHandler(.success(responseModel))
-            } catch {
-                completionHandler(.failure(error))
-            }
-        }.resume()
-    }
-    
-    func getSchoolDetails(completionHandler: @escaping (Result<[SchoolDetails], Error>) -> Void) {
-        guard let url = URL(string: schoolDetailsServiceURL) else {
+
+    func fetchData<T: Decodable>(from urlString: String, responseType: T.Type, completionHandler: @escaping (Result<T, Error>) -> Void) {
+        guard let url = URL(string: urlString) else {
             completionHandler(.failure(NSError(domain: "", code: 404, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
             return
         }
@@ -59,7 +31,7 @@ class SchoolService {
             
             do {
                 let jsonDecoder = JSONDecoder()
-                let responseModel = try jsonDecoder.decode([SchoolDetails].self, from: unwrappedData)
+                let responseModel = try jsonDecoder.decode(T.self, from: unwrappedData)
                 completionHandler(.success(responseModel))
             } catch {
                 completionHandler(.failure(error))
@@ -67,5 +39,11 @@ class SchoolService {
         }.resume()
     }
     
+    func getSchools(completionHandler: @escaping (Result<[School], Error>) -> Void) {
+        fetchData(from: schoolServiceURL, responseType: [School].self, completionHandler: completionHandler)
+    }
     
+    func getSchoolDetails(completionHandler: @escaping (Result<[SchoolDetails], Error>) -> Void) {
+        fetchData(from: schoolDetailsServiceURL, responseType: [SchoolDetails].self, completionHandler: completionHandler)
+    }
 }
